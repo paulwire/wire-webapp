9f90105af57d1d2b5787708317e9434169578fae
Paul <paulhinterberger@gmail.com>
2026-02-17 10:56:22 +0100

    feat(thread-ui): show dedicated active thread icon in title bar

--- apps/webapp/src/script/components/TitleBar/TitleBar.tsx
+++ apps/webapp/src/script/components/TitleBar/TitleBar.tsx
@@ -155,6 +155,10 @@ export const TitleBar = ({
   const smBreakpoint = useMatchMedia(QUERY.tabletSMDown);
 
   const {close: closeRightSidebar} = useAppMainState(state => state.rightSidebar);
+  const activeRightSidebarPanel = useAppMainState(state => {
+    const {history} = state.rightSidebar;
+    return history[history.length - 1];
+  });
 
   const {setCurrentView: setView} = useAppMainState(state => state.responsiveView);
 
@@ -244,6 +248,8 @@ export const TitleBar = ({
   }, [activeCalls.length]);
 
   const {showStartedCallAlert} = useCallAlertState();
+  const isThreadPanelOpen = activeRightSidebarPanel === PanelState.MESSAGE_THREAD;
+  const isInfoPanelActive = isRightSidebarOpen && !isThreadPanelOpen;
 
   return (
     <ul
@@ -361,16 +367,30 @@ export const TitleBar = ({
             )}
           </>
         ) : (
-          <button
-            type="button"
-            title={t('tooltipConversationInfo')}
-            aria-label={t('tooltipConversationInfo')}
-            onClick={onClickDetails}
-            className={cx('conversation-title-bar-icon', {active: isRightSidebarOpen})}
-            data-uie-name="do-open-info"
-          >
-            <Icon.InfoIcon />
-          </button>
+          <>
+            {isThreadPanelOpen && (
+              <button
+                type="button"
+                title="Thread"
+                aria-label="Thread"
+                className={cx('conversation-title-bar-icon', {active: isThreadPanelOpen})}
+                data-uie-name="status-open-thread"
+                tabIndex={TabIndex.UNFOCUSABLE}
+              >
+                <Icon.MessageIcon />
+              </button>
+            )}
+            <button
+              type="button"
+              title={t('tooltipConversationInfo')}
+              aria-label={t('tooltipConversationInfo')}
+              onClick={onClickDetails}
+              className={cx('conversation-title-bar-icon', {active: isInfoPanelActive})}
+              data-uie-name="do-open-info"
+            >
+              <Icon.InfoIcon />
+            </button>
+          </>
         )}
       </li>
 
